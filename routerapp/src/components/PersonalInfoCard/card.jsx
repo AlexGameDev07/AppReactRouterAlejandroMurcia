0072aacfbbcad1dde0ce3c0ef80038417042e967
AlexDev07 <alekeymurcia@gmail.com>
2025-03-25 14:05:59 -0600

    add PersonalInfoCard component to display student information

---
+++ routerapp/src/components/PersonalInfoCard/card.jsx
@@ -0,0 +1,22 @@
+function callCard() {
+    return (
+
+        <div class="row">
+            <div class="col s12 m6">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="https://student-photographs.s3.us-east-2.amazonaws.com/20230205.png?X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAVL5BYAQGNBT62GM4%2F20250318%2Fus-east-2%2Fs3%2Faws4_request&X-Amz-Date=20250318T205550Z&X-Amz-SignedHeaders=host&X-Amz-Expires=86400&X-Amz-Signature=ad2d922cdc9beb151d28dbf3914d16c08fd7812000054160a02014d78cb4d30e"/>
+                            <span class="card-title">Héctor Alejandro Murcia Rivera</span>
+                            
+                    </div>
+                    <div class="card-content">
+                        <p>Estudiante del ITR, vivo en mi casita, tengo 18 años de edad, mi código es 20230205, cumplo el 24 de noviembre.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default callCard;
