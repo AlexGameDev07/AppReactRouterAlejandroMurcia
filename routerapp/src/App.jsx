38e05eb4869596795c802f726be950f20fa6c16c
AlexDev07 <alekeymurcia@gmail.com>
2025-03-25 13:49:01 -0600

    add Bootstrap integration and implement routing with multiple pages

--- routerapp/src/App.jsx
+++ routerapp/src/App.jsx
@@ -1,33 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {BrowserRouter as Router, Routes, Route} from 'react-router'
 import './App.css'
 
+import Home from './pages/Home'
+import BootstrapPage from './pages/BootstrapPage'
+import Calculator from './pages/Calculator'
+import InfoCard from './pages/InfoCard'
+import StudentsForm from './pages/StudentsForm'
+import TodoList from './pages/TodoList'
+
+import Nav from './components/Nav'
+import Footer from './components/Footer'
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Router>
+        <Nav/>
+        <Routes>
+          <Route path="/"element={<Home />} />
+          <Route path="/bootstrap" element={<BootstrapPage />} />
+          <Route path="/calculator" element={<Calculator />} />
+          <Route path="/info-card" element={<InfoCard />} />
+          <Route path="/students-form" element={<StudentsForm />} />
+          <Route path="/todo-list" element={<TodoList />} />
+        </Routes>
+        <Footer/>
+      </Router>
     </>
   )
 }
---
+++ routerapp/src/components/Nav.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+
+const Nav = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="/">
+                    ReactRouter
+                </a>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNav"
+                    aria-controls="navbarNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="/">
+                                Inicio
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/bootstrap">
+                                20 Componentes
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/info-card">
+                                Card con InfoCard
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/calculator">
+                                Calculadora
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/todo-list">
+                                Todo List
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/students-form">
+                                Formulario de estudiantes
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Nav;
